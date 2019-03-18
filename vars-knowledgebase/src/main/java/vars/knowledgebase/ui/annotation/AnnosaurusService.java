dbd2c00f2ab764de7560fe11433c1f37d488041d
Brian Schlining <bschlining@gmail.com>
2019-03-18 14:45:22 -0700

    Addresses Issues #1 and #2

--- vars-knowledgebase/src/main/java/vars/knowledgebase/ui/annotation/AnnosaurusService.java
+++ vars-knowledgebase/src/main/java/vars/knowledgebase/ui/annotation/AnnosaurusService.java
@@ -1,8 +1,14 @@
 package vars.knowledgebase.ui.annotation;
 
+import com.typesafe.config.Config;
 import org.mbari.m3.corelib.Initializer;
+import org.mbari.m3.corelib.model.Authorization;
 import org.mbari.m3.corelib.model.ConceptCount;
 import org.mbari.m3.corelib.model.ConceptsRenamed;
+import org.mbari.m3.corelib.services.AuthService;
+import org.mbari.m3.corelib.services.BasicJWTAuthService;
+import org.mbari.m3.corelib.services.annosaurus.v1.AnnoService;
+import org.mbari.m3.corelib.services.annosaurus.v1.AnnoWebServiceFactory;
 import org.mbari.m3.corelib.util.AsyncUtils;
 
 import java.time.Duration;
@@ -17,9 +23,21 @@ import java.util.function.Function;
  */
 public class AnnosaurusService implements AnnotationService {
 
-    private org.mbari.m3.corelib.services.AnnotationService annotationService =
-            Initializer.getInjector()
-                    .getInstance(org.mbari.m3.corelib.services.AnnotationService.class);
+    private final org.mbari.m3.corelib.services.AnnotationService annotationService;
+
+    public AnnosaurusService() {
+
+        Config config = Initializer.getConfig();
+        String endpoint = config.getString("annotation.service.url");
+        String clientSecret = config.getString("annotation.service.client.secret");
+        Duration timeout = config.getDuration("annotation.service.timeout");
+        AnnoWebServiceFactory factory = new AnnoWebServiceFactory(endpoint, timeout);
+        AuthService authService = new BasicJWTAuthService(factory,
+                new Authorization("APIKEY", clientSecret));
+
+        this.annotationService = new AnnoService(new AnnoWebServiceFactory(endpoint, timeout),
+                authService);
+    }
 
     @Override
     public void updateConceptUsedByAnnotations(String newConcept, Collection<String> oldConcepts) {
