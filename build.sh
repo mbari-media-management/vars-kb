00d886c65a8647a519eafa99f1d11d382f65be96
Brian Schlining <bschlining@gmail.com>
2020-06-22 20:02:58 -0700

    Added codesigning for macs

--- build.sh
+++ build.sh
@@ -5,4 +5,3 @@
 export JPACKAGE_HOME="/Library/Java/JavaVirtualMachines/adoptopenjdk-14.jdk/Contents/Home"
 source "$HOME/workspace/m3-deployspace/vars-kb/env-config.sh"
 gradlew clean jpackage --info
-ls 
