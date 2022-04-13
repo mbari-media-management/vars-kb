10b946ec5cd4bdfb17971217fc7ea6a8fa9f7363
Brian Schlining <bschlining@gmail.com>
2022-04-13 08:27:05 -0700

    Check point

--- org.mbari.kb.jpa/src/main/java/org/mbari/kb/jpa/EntityManagerFactories.java
+++ org.mbari.kb.jpa/src/main/java/org/mbari/kb/jpa/EntityManagerFactories.java
@@ -5,6 +5,7 @@ import com.typesafe.config.ConfigFactory;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,6 +26,32 @@ public class EntityManagerFactories {
             "javax.persistence.schema-generation.database.action", "create"
     );
 
+    public static EntityManagerFactory newEntityManagerFactory(String url,
+                                                               String username,
+                                                               String password,
+                                                               String driverName,
+                                                               String productName,
+                                                               Map<String, String> properties) {
+        Map<String, String> props = new HashMap<>(properties);
+        props.put("javax.persistence.jdbc.url", url);
+        props.put("javax.persistence.jdbc.user", username);
+        props.put("javax.persistence.jdbc.password", password);
+        props.put("javax.persistence.jdbc.driver", driverName);
+        props.put("eclipselink.target-database", productName);
+        props.put("javax.persistence.database-product-name", productName);
+
+        String logLevel = config.getString("database.loglevel");
+        props.put("eclipselink.logging.level", logLevel);
+
+       return newEntityManagerFactory(props);
+    }
+
+    private static EntityManagerFactory newEntityManagerFactory(Map<String, String> properties) {
+        Map<String, String> props = new HashMap<>(productionProps);
+        props.putAll(properties);
+        return Persistence.createEntityManagerFactory("vars-jpa-knowledgebase", props);
+    }
+
 
     public static EntityManagerFactory newEntityManagerFactory(String configNode) {
         String driver = config.getString(configNode + ".driver");
@@ -33,17 +60,6 @@ public class EntityManagerFactories {
         String password = config.getString(configNode + ".password");
         String productName = config.getString(configNode + ".name");
 
-        Map<String, String> props = new HashMap<>(productionProps);
-        props.put("javax.persistence.jdbc.url", url);
-        props.put("javax.persistence.jdbc.user", user);
-        props.put("javax.persistence.jdbc.password", password);
-        props.put("javax.persistence.jdbc.driver", driver);
-        props.put("eclipselink.target-database", productName);
-        props.put("javax.persistence.database-product-name", productName);
-
-        String logLevel = config.getString("database.loglevel");
-        props.put("eclipselink.logging.level", logLevel);
-
-        return Persistence.createEntityManagerFactory("vars-jpa-knowledgebase", props);
+       return newEntityManagerFactory(url, user, password, driver, productName, Collections.emptyMap());
     }
 }
